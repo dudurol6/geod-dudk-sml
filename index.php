287d5c7c6b420d7335228f246ca27c1cb5bc82c3
Maciej Dudkowski <dudurol66@gmail.com>
2023-07-23 21:29:54 +0200

    the work so far (23.07)

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
+    <meta name="author" content="Maciej Dudkowski">
+    <meta name="description" content="Biuro Usług Geodezyjnych Dudkowski - jakość, profesjonalizm oraz indywidualne podejście do klienta są podstawą Naszej działalności.">
+    <meta name="keywords" content="geodeta, usługi geodezyjne, dudkowski, biuro usług geodezyjnych, geodeta lubelskie, lubelskie, lublin, bełżyce, opinie dla sądu, biegły sądowy z zakresu geodezji">    
+    <title>Strona Główna - BIURO USŁUG GEODEZYJNYCH DUDKOWSKI</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400;1,700&family=Pathway+Extreme:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header class="container-fluid" style="position: sticky; top: 0; background-color: #ffffff;">
+        <div class="row align-items-center justify-content-center f-md">
+            <div class="col-xs-12 col-sm-3" align="center"><img src="img/company-logo.png" class="company-logo"></div>
+            <div class="col-xs-12 col-sm-9" align="center">
+                <ul class="vertical-nav">
+                    <li><a href="#" class="header-nav-list-item"><span style="color: #ff2b00;">Strona Główna</span></a></li>
+                    <li><a href="#" class="header-nav-list-item">Oferta</a></li>
+                    <li><a href="#" class="header-nav-list-item">O firmie</a></li>
+                    <li><a href="#" class="header-nav-list-item">Kontakt</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="homepage-top">
+            
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+</body>
+</html>
